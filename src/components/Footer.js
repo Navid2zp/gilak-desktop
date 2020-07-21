25c745f44b28f8e688926d4cb9e36e7291b198f3
Navid Zarepak <navid2zp@gmail.com>
2020-07-21 18:38:28 +0430

    add android link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,30 @@ const electron = window.require("electron");
 
 function Footer() {
   return (
-      <footer className="page-footer pt-5">
-        <p className="text-center footer-text" style={{ width: "100%" }}>
-          Design by{" "}
-          <a
-            href="https://navid2zp.com"
-            onClick={(evt) => {
-              evt.preventDefault();
-              electron.shell.openExternal("https://navid2zp.com");
-            }}
-          >
-            Navid Zarepak
-          </a>{" "}
-          - Source: gdic.ir
-        </p>
-      </footer>
+    <footer className="page-footer pt-4 text-center">
+      <img
+        src="./static/android-icon.png"
+        alt="Android Version"
+        width="48"
+        height="48"
+		title="دریافت نسخه اندروید"
+		style={{cursor: "pointer"}}
+		onClick={() => electron.shell.openExternal("https://play.google.com/store/apps/details?id=studio.arvix.gilak")}
+      />
+      <p className="text-center footer-text mt-1" style={{ width: "100%" }}>
+        Design by{" "}
+        <a
+          href="https://navid2zp.com"
+          onClick={(evt) => {
+            evt.preventDefault();
+            electron.shell.openExternal("https://navid2zp.com");
+          }}
+        >
+          Navid Zarepak
+        </a>{" "}
+        - Source: gdic.ir
+      </p>
+    </footer>
   );
 }
 
